bcb414bd23b6aa1967e78b3dddd9159575cf9097
Rohan <RohanZarathustra@gmail.com>
2023-09-19 16:17:39 -0400

    Rohan Nero is Tailwind CSS

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -20,45 +20,47 @@ export default function Home() {
         </div>
 
         <div className="flex justify-evenly font-mono bg-green-300 py-32 w-full h-[69vh]">
+          {/* User component */}
           <Link className="w-1/3" href="/user">
             {/* Gold coin 1 container */}
-            <div className="relative flex items-center justify-center h-96 w-96 bg-gradient-to-br from-yellow-200 via-yellow-500 to-yellow-300 shadow-lg rounded-full border-8 border-yellow-500 hover:-translate-y-2 hover:shadow-xl">
+            <div className="relative flex items-center justify-center h-96 w-96 bg-gradient-to-br from-yellow-200 via-yellow-500 to-yellow-300 shadow-lg rounded-full border-8 border-yellow-500 hover:-translate-y-2 hover:shadow-xl hover:from-yellow-200 hover:via-yellow-500 hover:from-15% hover:to-85% hover:to-yellow-200">
               {/* Four Leaf Clover */}
-              <div className="h-32 w-32 relative flex items-center justify-center">
+              <div className="h-1/3 w-1/3 relative flex items-center justify-center">
                 {/* Main Square */}
-                <div className="absolute h-full w-full bg-green-600"></div>
+                <div className="absolute h-full w-full bg-green-700"></div>
                 {/* Leaf 1 left */}
-                <div className="absolute h-1/2 w-1/2 rounded-full bg-green-600 -left-[25%] top-0"></div>
+                <div className="absolute h-1/2 w-1/2 rounded-full bg-green-700 -left-[25%] top-0"></div>
                 {/* Leaf 1 right */}
-                <div className="absolute h-1/2 w-1/2 rounded-full bg-green-600 -left-[.1%] -top-[25%]"></div>
+                <div className="absolute h-1/2 w-1/2 rounded-full bg-green-700 -left-[.1%] -top-[25%]"></div>
                 {/* Leaf 2 left */}
-                <div className="absolute h-1/2 w-1/2 rounded-full bg-green-600 -right-[25%] top-0"></div>
+                <div className="absolute h-1/2 w-1/2 rounded-full bg-green-700 -right-[25%] top-0"></div>
                 {/* Leaf 2 right */}
-                <div className="absolute h-1/2 w-1/2 rounded-full bg-green-600 -right-[.1%] -top-[25%]"></div>
+                <div className="absolute h-1/2 w-1/2 rounded-full bg-green-700 -right-[.1%] -top-[25%]"></div>
                 {/* Leaf 3 left */}
-                <div className="absolute h-1/2 w-1/2 rounded-full bg-green-600 -left-[25%] bottom-0"></div>
+                <div className="absolute h-1/2 w-1/2 rounded-full bg-green-700 -left-[25%] bottom-0"></div>
                 {/* Leaf 3 right */}
-                <div className="absolute h-1/2 w-1/2 rounded-full bg-green-600 -left-[.1%] -bottom-[25%]"></div>
+                <div className="absolute h-1/2 w-1/2 rounded-full bg-green-700 -left-[.1%] -bottom-[25%]"></div>
                 {/* Leaf 4 left */}
-                <div className="absolute h-1/2 w-1/2 rounded-full bg-green-600 -right-[25%] bottom-0"></div>
+                <div className="absolute h-1/2 w-1/2 rounded-full bg-green-700 -right-[25%] bottom-0"></div>
                 {/* Leaf 4 right */}
-                <div className="absolute h-1/2 w-1/2 rounded-full bg-green-600 -right-[.1%] -bottom-[25%]"></div>
+                <div className="absolute h-1/2 w-1/2 rounded-full bg-green-700 -right-[.1%] -bottom-[25%]"></div>
                 {/* Stem */}
-                <div className="absolute h-2/3 w-1/12 rotate-6 bg-green-600 rounded-br-lg -bottom-1/2 left-1/2 transform -translate-x-1/2"></div>
+                <div className="absolute h-2/3 w-1/12 rotate-6 bg-green-700 rounded-br-lg -bottom-1/2 left-1/2 transform -translate-x-1/2"></div>
               </div>
             </div>
           </Link>
 
+          {/* Organizer component */}
           <Link className="w-1/3" href="/organizer">
             {/* The pot */}
             <div className="relative w-96 h-96  bg-gray-800 rounded-full z-10">
               {/* The gold coins */}
-              <div className="rounded-full h-8 w-8 bg-gradient-to-br from-yellow-300 via-yellow-400 to-yellow-300 shadow-lg border-4 border-yellow-400 absolute -top-4 left-[75%]"></div>
-              <div className="rounded-full h-8 w-8 bg-gradient-to-br from-yellow-300 via-yellow-400 to-yellow-300 shadow-lg border-4 border-yellow-400 absolute -top-4 left-[50%]"></div>
+              <div className="rounded-full h-8 w-8 bg-gradient-to-br from-yellow-200 via-yellow-500 to-yellow-300 shadow-lg border-4 border-yellow-400 absolute -top-4 left-[75%]"></div>
+              <div className="rounded-full h-8 w-8 bg-gradient-to-br from-yellow-200 via-yellow-500 to-yellow-300 shadow-lg border-4 border-yellow-400 absolute -top-4 left-[50%]"></div>
               {/* Gold coin 1 container */}
               <div className="relative">
                 {/* Gold Coin */}
-                <div className="rounded-full h-12 w-12 bg-gradient-to-br from-yellow-300 via-yellow-400 to-yellow-300 shadow-lg border-4 border-yellow-400 absolute -top-4 left-[25%]"></div>
+                <div className="rounded-full h-12 w-12 bg-gradient-to-br from-yellow-200 via-yellow-500 to-yellow-300 shadow-lg border-4 border-yellow-400 absolute -top-4 left-[25%]"></div>
                 {/* Four Leaf Clover */}
                 <div className="h-4 w-4 relative flex items-center justify-center">
                   {/* Main Square */}
@@ -89,6 +91,7 @@ export default function Home() {
               <div className="w-5 h-6 bg-gray-100 rounded-full absolute top-28 right-3/4 transform translate-x-2/3"></div>
             </div>
           </Link>
+
           {/* Four Leaf Clover */}
           <div className="h-32 w-32 relative flex items-center justify-center bg-red-300">
             {/* Main Square */}
